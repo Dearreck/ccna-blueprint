c6d7d600c356fa052c776d5a2e40232fc63f03b4
Dearreck Escoto <dearreck.escoto@gmail.com>
2025-10-04 07:16:23 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggler = document.getElementById('theme-toggler');
+    const currentTheme = localStorage.getItem('theme');
 
+    // Aplicar el tema guardado al cargar la página
+    if (currentTheme) {
+        document.body.classList.add(currentTheme);
+    }
+
+    themeToggler.addEventListener('click', () => {
+        // Alternar la clase 'dark-mode'
+        document.body.classList.toggle('dark-mode');
+
+        // Guardar la preferencia en localStorage
+        let theme = 'light'; // Por defecto
+        if (document.body.classList.contains('dark-mode')) {
+            theme = 'dark-mode';
+        }
+        localStorage.setItem('theme', theme);
+    });
+});
